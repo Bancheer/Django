658c183c75f911d78fdc3b0292aa39ce6ae99728
Banch <andreypronko2002@gmail.com>
2024-04-14 02:15:47 +0200

    first commit

---
+++ quotes_project/users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('add/', views.add, name='add'),
+    path('chenge_password/', views.change_password, name='password'),
+]
